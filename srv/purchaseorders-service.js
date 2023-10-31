30a7fbf2127c2469cc9562747b5c30e8038c10e1
Steven Cao <steven.cao@ernox.com>
2023-10-31 06:53:00 +0000

    build generated

--- srv/purchaseorders-service.js
+++ srv/purchaseorders-service.js
@@ -2,5 +2,9 @@ const cds = require('@sap/cds');
 
 
 module.exports = cds.service.impl(async function () { 
-    
+    const gwservice = await cds.connect.to('gwsample');
+    let srv = this;
+    this.on('READ', 'Contacts', async(req, next) => {
+        return next();
+    })
 })
